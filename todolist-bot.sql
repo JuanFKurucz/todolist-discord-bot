ede0084f3637abea8ea5e953bf9ef129253b2d82
Juan Francisco Kurucz <juanfkurucz@gmail.com>
2018-12-09 14:14:29 -0300

    SQL dump

--- todolist-bot.sql
+++ todolist-bot.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.9
+-- version 4.8.3
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 09-12-2018 a las 03:51:42
--- Versión del servidor: 5.7.21
--- Versión de PHP: 5.6.35
+-- Tiempo de generación: 09-12-2018 a las 17:14:14
+-- Versión del servidor: 5.7.23
+-- Versión de PHP: 7.2.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -65,6 +65,60 @@ CREATE TABLE IF NOT EXISTS `activity_requires` (
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `guild`
+--
+
+DROP TABLE IF EXISTS `guild`;
+CREATE TABLE IF NOT EXISTS `guild` (
+  `id_guild` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_guild`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `guild`
+--
+
+INSERT INTO `guild` (`id_guild`) VALUES
+('509053465016795147');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `guild_permission`
+--
+
+DROP TABLE IF EXISTS `guild_permission`;
+CREATE TABLE IF NOT EXISTS `guild_permission` (
+  `id_guild` varchar(20) NOT NULL,
+  `id_role` varchar(20) NOT NULL,
+  `command` varchar(50) NOT NULL,
+  PRIMARY KEY (`id_guild`,`command`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id_role` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(350) NOT NULL,
+  `permission` int(11) NOT NULL,
+  PRIMARY KEY (`id_role`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `role`
+--
+
+INSERT INTO `role` (`id_role`, `name`, `permission`) VALUES
+(1, 'Admin', 2);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `task`
 --
 
@@ -102,7 +156,6 @@ CREATE TABLE IF NOT EXISTS `task_requires` (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id_user` varchar(20) NOT NULL,
-  `permission` int(11) NOT NULL,
   PRIMARY KEY (`id_user`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
@@ -110,8 +163,30 @@ CREATE TABLE IF NOT EXISTS `user` (
 -- Volcado de datos para la tabla `user`
 --
 
-INSERT INTO `user` (`id_user`, `permission`) VALUES
-('162355874570960896', 1);
+INSERT INTO `user` (`id_user`) VALUES
+('162355874570960896'),
+('259426549693022218');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `user_guild_role`
+--
+
+DROP TABLE IF EXISTS `user_guild_role`;
+CREATE TABLE IF NOT EXISTS `user_guild_role` (
+  `id_user` varchar(20) NOT NULL,
+  `id_guild` varchar(20) NOT NULL,
+  `id_role` int(11) NOT NULL,
+  PRIMARY KEY (`id_user`,`id_guild`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Volcado de datos para la tabla `user_guild_role`
+--
+
+INSERT INTO `user_guild_role` (`id_user`, `id_guild`, `id_role`) VALUES
+('162355874570960896', '509053465016795147', 1);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
